0ea71f97ceaac7e4942fd6615b53d6633dec3425
DVCF <47047103+datboimech@users.noreply.github.com>
2020-03-31 16:04:46 -0500

    practice

---
+++ app.js
@@ -0,0 +1,120 @@
+'use strict'
+
+// let a = 2 + 9;
+
+// switch (a) 
+// {
+//   case 2:
+//   case 3:
+//     alert( 'Nood' );
+    
+   //break;
+  
+//   case 4:
+//     alert( 'Normal' );
+    
+//     break;
+  
+//   case 5:
+//   case 6: 
+//     alert( 'Decent Pro' );
+    
+//     break;
+  
+//    default:
+    
+//     alert( "GOD!" );
+// }
+
+
+
+// let i = 3;
+
+// while (i < 10)  // shows 0, then 1, then 2
+// {
+//   alert( i );
+//   i++;
+// }
+
+
+// let i = 11;
+
+// while (i)  // shows 0, then 1, then 2
+// {
+//   alert( i );
+//   i--;
+// }
+
+
+// let userName = prompt(`What is your name.`)
+
+//     alert(`welcome ${userName}`)
+
+
+// let number = prompt("what is this 5102", '');
+
+// if (number == 2015) 
+// {
+//   alert( 'You guessed it right!' );
+// } 
+// else 
+// {
+//   alert( 'good tried' );
+// }
+
+/*
+let user = prompt("Gamer tag", '');
+
+if ( user == Max) 
+{
+  alert( `Welcome back ${user}` );
+} 
+else 
+{
+  alert( `Welcome unknow` ); 
+}
+*/
+// This failed ALSO A TEST RUN
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
